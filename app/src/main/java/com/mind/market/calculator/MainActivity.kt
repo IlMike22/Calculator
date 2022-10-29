6de35fae0989beeef77a8fa45bf782dc78cf8abb
IlMike22 <michaelwidlok@yahoo.de>
2022-10-29 09:41:12 +0200

    added some build variants and an own Constants file

--- app/src/main/java/com/mind/market/calculator/MainActivity.kt
+++ app/src/main/java/com/mind/market/calculator/MainActivity.kt
@@ -3,7 +3,6 @@ package com.mind.market.calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.mind.market.calculator.presentation.CalculatorDisplay
 import com.mind.market.calculator.presentation.CalculatorScreen
 import com.mind.market.calculator.ui.theme.MaterialCalculatorTheme
 
@@ -13,6 +12,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MaterialCalculatorTheme {
                 CalculatorScreen()
+                Constants.BASE_URL
             }
         }
     }
---
+++ app/src/release/java/com/mind/market/calculator/Constants.kt
@@ -0,0 +1,5 @@
+package com.mind.market.calculator
+
+object Constants {
+    const val BASE_URL = "https://mind-market.de"
+}
